99e7f0713ed8ea1265ccf9ee2bed599b4ee13d4e
Sanketrb2004 <139944677+Sanketrb2004@users.noreply.github.com>
2025-02-16 10:32:01 +0000

    container config done

---
+++ basics/02_datatypes.js
@@ -0,0 +1,13 @@
+// number
+//string=""
+//bigint= used for big numeric values 
+// boolean = true/false 
+// Null = Standalone value 
+// undefined = 
+// symbol =  unique ness
+// object 
+console.log(typeof string )
+let name ="Sanket"
+console.log(typeof name)
+console.log(typeof undefined)
+console.log(typeof null)
--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,7 @@
 console.log("Sanket")
+let accountName = "Sanket"
+let accbranch = "Mahabal"
+const accId = "432"
+accCity ="jaipur"
+let atm_number;
+console.table([accountName,accbranch,accId,accCity,atm_number])
